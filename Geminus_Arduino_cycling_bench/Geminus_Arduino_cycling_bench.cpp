74218e9af3494bbec2c995d844eb2b5a89ac6ea5
ArduinoGeminus <ArduinoGeminus@SteinLaptop-Pc>
2021-02-18 21:48:32 -0700

    Count from 50 000 cycles, only prints every 10 counts.

--- Geminus_Arduino_cycling_bench/Geminus_Arduino_cycling_bench.cpp
+++ Geminus_Arduino_cycling_bench/Geminus_Arduino_cycling_bench.cpp
@@ -15,7 +15,7 @@ unsigned long AUX1 = 0;                 // Pulse high timer variable
 
 unsigned long AUX2 = 0;                 // Pulse low timer variable
 unsigned long AUX3 = 0;                 // Pulse high timer variable
-Counter ctr(100, 1);                    // Counts down, starting at 100
+Counter ctr(50000, 1);                    // Counts down, starting at 100
 
 void setup() {
   setupPLC();                           // Define inputs and outputs
@@ -26,19 +26,28 @@ void setup() {
 
 void loop() {
 if (X0){                          // Read Enable input (1 = enable)
-  inNot ((ctr.lowerQ()));
-  inNot(timerCycle(AUX0, 100, AUX1, 100));     // Repeating pulse, low = 0.9 s, high = 0.1 s
-  out(Y0);                              // Send pulse waveform to Output 0
-  ctr.countDown();                      // Count down
+	  inNot ((ctr.lowerQ()));
+	  inNot(timerCycle(AUX0, 200, AUX1, 300));     // Repeating pulse, low = 0.1 s, high = 0.1 s
+	  out(Y0);                              // Send pulse waveform to Output 0
+	  ctr.countDown();                      // Count down
 
 
-//  ctr.lowerQ();            // Display Count Up output on Y1
-//  out(Y1);
-/*  inNot (X0);                           // Read Enable input (1 = enable)
-  timerCycle(AUX2, 750, AUX3, 500);     // Repeating pulse, low = 0.9 s, high = 0.1 s
-  out(Y1);                              // Send pulse waveform to Output 0
-  ctr.countDown();                      // Count down
-*/
-  Serial.println(ctr.count());          // Send count to serial port
-}
+	//  ctr.lowerQ();            // Display Count Up output on Y1
+	//  out(Y1);
+	/*  inNot (X0);                           // Read Enable input (1 = enable)
+	  timerCycle(AUX2, 750, AUX3, 500);     // Repeating pulse, low = 0.9 s, high = 0.1 s
+	  out(Y1);                              // Send pulse waveform to Output 0
+	  ctr.countDown();                      // Count down
+	*/
+	  static int i=0;
+	  static int lastcount=0;
+	  if(lastcount!=ctr.count()){
+		  lastcount=ctr.count();
+		  i++;
+		  if(i>=10){
+			  Serial.println(ctr.count());          // Send count to serial port
+			  i=0;
+		  }
+	  }
+	}
 }
